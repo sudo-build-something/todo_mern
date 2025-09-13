39ac69891a172aedf7e395f6e0b4f8461c4114ee
sudo-build-something <mkeamogetswe17@gmail.com>
2025-09-13 14:09:58 +0200

    Refactor Register and Login controllers for error reporting to user.
    Login: Identify server errors that should not be leaked and user errors that should not be too specific (security feature).
    Register: Identify server errors that should not be leaked. Handler is needed to split errors.

--- backend/controllers/loginController.js
+++ backend/controllers/loginController.js
@@ -3,22 +3,32 @@ const User = require("../models/User");
 
 
 exports.getLogin = async (req, res) => {
-    res.render("login");
+    var errors = {};
+    res.render("login", { errors });
 };
 
 exports.submitLogin = async (req, res) => {
-    auth.authenticate("local", (err, user) => {
+    var errors = {}
+    auth.authenticate("local", (err, user, info, status) => {
         if (err) {
+            //TODO: Backend errors, log on the server-side (do not report to user).
+            //Use appropriate status code.
             console.log(err);
+            errors.message = "Something went wrong, please try again later.";
+            return res.render("login", { errors });
         }
         if (!user) {
-            console.log("Invalid username or password");
-            return;
+            //TODO: Log the detailed error on the server-side.
+            errors.message = "Invalid credentials."
+            return res.render("login", { errors });
         }
-        req.logIn(user, (err) =>{
+        req.logIn(user, (err) => {
             if (err) {
+                //TODO: Backend errors, log on the server-side (do not report to user).
+                // Use appropriate status code.
                 console.log(err)
-                return;
+                errors.message = "Something went wrong, please try again later.";
+                return res.render("login", { errors });
             }
             res.redirect("/");
         });
--- backend/controllers/registerController.js
+++ backend/controllers/registerController.js
@@ -8,6 +8,7 @@ exports.getRegister = async (req, res) => {
 };
 
 exports.submitRegister = async (req, res) => {
+  //TODO: Move field validation to the client side and report under fields.
   const { valid, errors } = userValidator.validateRegistrationInput(req.body);
   const {firstname, lastname, username, email, password } = req.body;
   if (valid) {
@@ -18,15 +19,17 @@ exports.submitRegister = async (req, res) => {
       email: email
     });
     
+    //TODO: Report these errors in modals given backend processing.
     User.register(user, password, (err) => {
+      //TODO: Handle UserExists, Validation, Connection & password errors.
+      //Implement utility error handler.
       if (err) {
-        console.log(err);
-        return res.redirect("register", { errors });
+        return res.redirect("register");
       }
       return res.redirect("login");
     });
   
   } else {
-    res.render("register", { errors });
+    return res.render("register", { errors });
   }
 };
